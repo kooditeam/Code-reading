65afc93c8728e7113c055828befe5b03b98a7bfc
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-06-11 16:23:45 +0300

    html file (primarily for testing purposes)

---
+++ src/main/java/codereading/controller/IndexController.java
@@ -0,0 +1,15 @@
+
+package codereading.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String mainPage() {
+        return "templates/index.html";
+    }
+}

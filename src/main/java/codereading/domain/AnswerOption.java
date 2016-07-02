4c83161faad36cc306c5d53399db6eba2279ead0
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-07-02 23:43:13 +0300

    answer option's question field serialized only as its id

--- src/main/java/codereading/domain/AnswerOption.java
+++ src/main/java/codereading/domain/AnswerOption.java
@@ -1,6 +1,10 @@
 
 package codereading.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import org.springframework.data.jpa.domain.AbstractPersistable;
@@ -8,11 +12,15 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 /**
  * Answer option belongs to question, which can have multiple answer options.
  * Each answer option has text to tell the user what the option is, and info
- * whether the answer option is the correct one to the question or not. 
+ * whether the answer option is the correct one to the question or not. When
+ * an answer option is serialized to JSON, its question field's info is 
+ * serialized only as its id.
  */
 @Entity
 public class AnswerOption extends AbstractPersistable<Long> {
 
+    @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
+    @JsonIdentityReference(alwaysAsId=true)
     @ManyToOne
     private Question question;
 

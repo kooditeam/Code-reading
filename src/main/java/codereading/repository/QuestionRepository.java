9fd9351d63e6301459ef15d5e5fb13c14bfe4ac9
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-07-03 23:25:27 +0300

    changed query to get unanswered questions to return both unanswered and wrongly answered

--- src/main/java/codereading/repository/QuestionRepository.java
+++ src/main/java/codereading/repository/QuestionRepository.java
@@ -1,6 +1,7 @@
 
 package codereading.repository;
 
+import codereading.domain.AnswerOption;
 import codereading.domain.Question;
 
 import java.util.List;
@@ -11,13 +12,14 @@ import org.springframework.data.repository.query.Param;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     /**
-     * Fetches all questions that have not been answered by a given user
-     * identified by their id.
+     * Fetches all questions that have not been answered correctly by a given 
+     * user identified by their id.
      * @param userId id of a given user
      * @return Questions not answered by a user
      */
     @Query("SELECT q FROM Question q WHERE q.id NOT IN "
-            + "(SELECT a.answerOption.question.id FROM Answer a WHERE a.answerer.id = :userId)")
-    List<Question> questionsNotAnsweredByUser(@Param("userId") Long userId);
+            + "(SELECT a.answerOption.question.id FROM Answer a WHERE a.answerer.id = :userId "
+            + "and a.answerOption.isCorrect != false)")
+    List<Question> questionsNotAnsweredCorrectly(@Param("userId") Long userId);
 
 }
--- src/test/java/codereading/repository/QuestionRepositoryTest.java
+++ src/test/java/codereading/repository/QuestionRepositoryTest.java
@@ -41,16 +41,33 @@ public class QuestionRepositoryTest {
         User user = saveUser();
 
         assertTrue(questionRepository.count() == 0);
-        assertTrue(questionRepository.questionsNotAnsweredByUser(user.getId()).isEmpty());
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).isEmpty());
     }
 
     @Test
     public void withOneQuestionInTheDatabaseAndUserNotHavingAnsweredAnyReturnsThatOneQuestion() {
         User user = saveUser();
         Question question = saveQuestion();
-        saveAnswer(question, user);
 
-        assertTrue(questionRepository.questionsNotAnsweredByUser(user.getId()).isEmpty());
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 1);
+    }
+
+    @Test
+    public void withOneQuestionInTheDatabaseAndUserHavingAnsweredThatWrongReturnsThatOneAnswer() {
+        User user = saveUser();
+        Question question = saveQuestion();
+        saveAnswer(question, user, false);
+
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 1);
+    }
+
+    @Test
+    public void withOneQuestionInTheDatabaseAndUserHavingAnsweredThatCorrectlyReturnsEmptyList() {
+        User user = saveUser();
+        Question question = saveQuestion();
+        saveAnswer(question, user, true);
+
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).isEmpty());
     }
 
     @Test
@@ -59,45 +76,94 @@ public class QuestionRepositoryTest {
         saveMultipleQuestions(5);
 
         assertTrue(questionRepository.count() == 5);
-        assertTrue(questionRepository.questionsNotAnsweredByUser(user.getId()).size() == 5);
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 5);
     }
 
     @Test
-    public void withMultipleQuestionsInTheDatabaseAndUserHavingAnsweredOnlyOneReturnsOnlyThatOneAnsweredQuestion() {
+    public void withMultipleQuestionsInTheDatabaseAndUserHavingAnsweredOnlyOneAndThatWasCorrectReturnsOnlyThatOneAnsweredQuestion() {
         User user = saveUser();
         Question question = saveMultipleQuestions(3).get(0);
 
-        Answer answer = saveAnswer(question, user);
+        Answer answer = saveAnswer(question, user, true);
         answerRepository.save(answer);
 
         assertTrue(questionRepository.count() == 3);
-        assertTrue(questionRepository.questionsNotAnsweredByUser(user.getId()).size() == 2);
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 2);
     }
 
     @Test
-    public void withAllQuestionsAnsweredByAUserTheyDoNotHaveAnyUnansweredQuestions() {
+    public void withMultipleQuestionsInDatabaseAndUserHavingAnsweredOnlyOneThatWasWrongReturnsEmptyList() {
         User user = saveUser();
-        answerQuestions(saveMultipleQuestions(4), user);
+        Question question = saveMultipleQuestions(3).get(0);
+
+        Answer answer = saveAnswer(question, user, false);
+        answerRepository.save(answer);
+
+        assertTrue(questionRepository.count() == 3);
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 3);
+    }
+
+    @Test
+    public void withAllQuestionsAnsweredByAUserCorrectlyTheUserDoesNotHaveAnyUnansweredQuestions() {
+        User user = saveUser();
+        answerQuestionsCorrectly(saveMultipleQuestions(4), user);
 
         assertTrue(questionRepository.count() == 4);
-        assertTrue(questionRepository.questionsNotAnsweredByUser(user.getId()).isEmpty());
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).isEmpty());
+    }
 
+    @Test
+    public void withAllQuestionsAnsweredByAUserWrongTheUserHasAllQuestionsStillUnansweredProperly() {
+        User user = saveUser();
+        answerQuestionsWrong(saveMultipleQuestions(4), user);
+        
+        assertTrue(questionRepository.count() == 4);
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 4);
+    }
+
+    @Test
+    public void someOfTheQuestionsAnsweredCorrectlyAndSomeWrongReturnsAllTheWronglyAnsweredQuestions() {
+        User user = saveUser();
+        answerQuestionsWrong(saveMultipleQuestions(3), user);
+        answerQuestionsCorrectly(saveMultipleQuestions(2), user);
+        
+        assertTrue(questionRepository.count() == 5);
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 3);
+    }
+
+    @Test
+    public void someOfTheQuestionsAnsweredCorrectlyAndSomeWrongAndSomeNotAtAllReturnsAllWronglyAndUnansweredQuestions() {
+        User user = saveUser();
+        answerQuestionsWrong(saveMultipleQuestions(3), user);
+        answerQuestionsCorrectly(saveMultipleQuestions(2), user);
+        saveMultipleQuestions(3);
+
+        assertTrue(questionRepository.count() == 8);
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user.getId()).size() == 6);
     }
 
     public void answeredQuestionsByAnotherUserAreNotCountedAsAnsweredQuestionsForSelf() {
         User user1 = saveUser();
         User user2 = saveUser();
 
-        answerQuestions(saveMultipleQuestions(2), user1);
+        answerQuestionsCorrectly(saveMultipleQuestions(2), user1);
 
         assertTrue(questionRepository.count() == 2);
-        assertTrue(questionRepository.questionsNotAnsweredByUser(user1.getId()).isEmpty());
-        assertTrue(questionRepository.questionsNotAnsweredByUser(user2.getId()).size() == 2);
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user1.getId()).isEmpty());
+        assertTrue(questionRepository.questionsNotAnsweredCorrectly(user2.getId()).size() == 2);
     }
 
-    private void answerQuestions(List<Question> questions, User user) {
+    private void answerQuestionsWrong(List<Question> questions, User user) {
+        answerQuestions(questions, user, false);
+    }
+
+    private void answerQuestionsCorrectly(List<Question> questions, User user) {
+        answerQuestions(questions, user, true);
+    }
+
+    private void answerQuestions(List<Question> questions, User user, boolean isCorrect) {
         for (Question question : questions) {
-            saveAnswer(question, user);
+            saveAnswer(question, user, isCorrect);
         }
     }
 
@@ -119,8 +185,8 @@ public class QuestionRepositoryTest {
         return userRepository.save(user);
     }
 
-    private Answer saveAnswer(Question question, User user) {
-        AnswerOption option = saveAnswerOption(question);
+    private Answer saveAnswer(Question question, User user, boolean isCorrect) {
+        AnswerOption option = saveAnswerOption(question, isCorrect);
 
         Answer answer = new Answer();
         answer.setAnswerOption(option);
@@ -129,8 +195,9 @@ public class QuestionRepositoryTest {
         return answerRepository.save(answer);
     }
 
-    private AnswerOption saveAnswerOption(Question question) {
+    private AnswerOption saveAnswerOption(Question question, boolean isCorrect) {
         AnswerOption option = new AnswerOption();
+        option.setIsCorrect(isCorrect);
         option.setQuestion(question);
         return answerOptionRepository.save(option);
     }
